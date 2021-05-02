bdc4b307e3ad3af19b626777d51382a0c0411bcd
StanMey <stan_1998@hotmail.com>
2021-05-03 00:46:41 +0200

    added latest docstrings

--- assignment2/utils.py
+++ assignment2/utils.py
@@ -1,4 +1,6 @@
 from typing import Tuple, List
+from collections import defaultdict
+ 
 import copy
 import random as rd
 import numpy as np
@@ -12,18 +14,21 @@ class Maze:
         args:
             R (np.ndarray): The array with the rewards per state.
             end (List[Tuple[int, int]]): A list with the positions of the end states.
-            start (Tuple)
+            start (Tuple[int, int]): The start state of the environment.
         """
         self.R = R
         self.end_states = end
         self.start_state = start
         self.states = np.arange(self.R.size).reshape(self.R.shape)
     
-    def get_next_action_positions(self, pos: Tuple[int, int]):
-        """asdf.
+    def get_next_action_positions(self, pos: Tuple[int, int]) -> Tuple[Tuple[int, int], Tuple[int, int], Tuple[int, int], Tuple[int, int]]:
+        """Get the next positions of the actions based on the current position.
         
         args:
-            pos (Tuple[int, int]): .
+            pos (Tuple[int, int]): The position for which to decide the next action positions.
+        
+        returns:
+            Tuple[Tuple[int, int], Tuple[int, int], Tuple[int, int], Tuple[int, int]]: The positions of the up, right, left and down actions.
         """
         row, col = pos
 
@@ -33,13 +38,27 @@ class Maze:
         down = (row + 1, col) if row + 1 < self.R.shape[0] else pos
         return up, right, left, down
     
-    def get_random_position(self):
-        """Returns a random position in the environment."""
-        return (rd.randint(0, self.R.shape[1] - 1), rd.randint(0, self.R.shape[0] - 1))
+    def get_random_position(self) -> Tuple[int, int]:
+        """Returns a random position in the environment.
+        
+        returns:
+            Tuple[int, int]: A random position in the environment.
+        """
+        return rd.randint(0, self.R.shape[1] - 1), rd.randint(0, self.R.shape[0] - 1)
 
 
-def value_iteration(env: Maze, p_action: float = 0.7, discount: float = 0.9, threshold: float = 0.0001):
-    """Value iteration method."""
+def value_iteration(env: Maze, p_action: float = 0.7, discount: float = 0.9, threshold: float = 0.0001) -> np.ndarray:
+    """A method for performing value iteration.
+    
+    args:
+        env (Maze): The environment which holds the rewards of all the possible states.
+        p_action (float, optional): The probability of a succesfull action. Defaults to 0.7.
+        discount (float, optional): The discount factor. Defaults to 0.9.
+        threshold (float, optional): The threshold for when the iteration is finished. Defaults to 0.0001.
+    
+    returns:
+        np.ndarray: Returns the utility of every state in the environment.
+    """
     
     utility = np.zeros(env.R.shape)
     delta = np.inf
@@ -62,7 +81,7 @@ def value_iteration(env: Maze, p_action: float = 0.7, discount: float = 0.9, thr
             actions = env.get_next_action_positions(pos)
             action_values = []
             for index, action in enumerate(actions):
-                noise_actions = actions[:index] + actions[index+1:]
+                noise_actions = list(actions[:index] + actions[index+1:])
                 action_values.append(calculate_action_value(env, utility, p_action, discount, action, noise_actions))
 
             # select the action with the highest utility
@@ -76,8 +95,20 @@ def value_iteration(env: Maze, p_action: float = 0.7, discount: float = 0.9, thr
     return utility
 
 
-def calculate_action_value(env: Maze, values, p_action: float, discount: float, action, other_actions) -> float:
-    """Calculates the action value of a certain action with the succesfullness probability of the actions."""
+def calculate_action_value(env: Maze, values: np.ndarray, p_action: float, discount: float, action: Tuple[int, int], other_actions: List[Tuple[int, int]]) -> float:
+    """Calculates the action value of a certain action with the succesfullness probability of the actions.
+    
+    args:
+        env (Maze): The environment which holds the rewards of all the possible states.
+        values (np.ndarray): The array with the values of the states.
+        p_action (float): The probability of a succesfull action.
+        discount (float): The discount factor.
+        action (Tuple[int, int]): The main action that will be taken.
+        other_actions (List[Tuple[int, int]]): The other actions that can be taken with the noise in play.
+    
+    returns:
+        float: Returns the value of the action.
+    """
     total_value = p_action * (env.R[action] + (discount * values[action]))
 
     dist_chance = (1 - p_action) / len(other_actions)
@@ -88,7 +119,11 @@ def calculate_action_value(env: Maze, values, p_action: float, discount: float, 
 
 
 def show_utility(values: np.ndarray):
-    """Prints the utility array to the screen."""
+    """Prints the utility array to the screen.
+    
+    args:
+        values (np.ndarray): The array with the values of the states.
+    """
     row_divider = "-" * ((8 * values.shape[0]) + values.shape[0] + 1)
     for row in range(values.shape[0]):
         print(row_divider)
@@ -99,8 +134,13 @@ def show_utility(values: np.ndarray):
     print(row_divider)
 
 
-def show_policy(env: Maze, q_table):
-        """Prints the q-table in a matplotlib figure."""
+def show_policy(env: Maze, q_table: defaultdict):
+        """Prints the q-table in a matplotlib figure.
+        
+        args:
+            env (Maze): The environment which holds the rewards of all the possible states.
+            q_table (defaultdict): The Q-table with the policy.
+        """
         plt.rc('figure', figsize=(12,6))
         for row in range(env.R.shape[0]):
             for col in range(env.R.shape[1]):
